2173ea752be88f45975584ccf3891d8509bd30c0
CMCDragonkai <cmcdragonkai@gmail.com>
2013-11-16 12:27:46 +1100

    php client should have api key, request parameters and api url

--- src/SnapSearchClientPHP/Client.php
+++ src/SnapSearchClientPHP/Client.php
@@ -4,8 +4,16 @@ namespace SnapSearchClientPHP;
 
 class Client{
 
+	protected $api_key;
+	protected $request_parameters;
+	protected $api_url;
+
+	public function __construct($api_key = false, array $request_parameters = null, $api_url = false){
+
+		$this->api_key = $api_key;
+		$this->request_parameters = ($request_parameters) ? $request_parameters : array();
+		$this->api_url = ($api_url) ? $api_url : 'http://snapsearch.io/api/v1/robot';
+
+	}
 	
-
-
-
 }
