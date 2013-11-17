2637a7a10437f75736fc5f4459f71b6c77ba30ac
CMCDragonkai <cmcdragonkai@gmail.com>
2013-11-17 15:05:31 +1100

    PHP will be using Symfony request and Httpful

--- src/SnapSearchClientPHP/Client.php
+++ src/SnapSearchClientPHP/Client.php
@@ -2,23 +2,76 @@
 
 namespace SnapSearchClientPHP;
 
+use Httpful\Request as Api;
+
 class Client{
 
 	protected $api_key;
 	protected $request_parameters;
 	protected $api_url;
+	protected $api;
+	protected $errors;
 
-	public function __construct($api_key = false, array $request_parameters = null, $api_url = false){
+	public function __construct(
+		$api_key, 
+		array $request_parameters = null, 
+		$api_url = false,
+		Api $api = null
+	){
 
 		$this->api_key = $api_key;
 		$this->request_parameters = ($request_parameters) ? $request_parameters : array();
 		$this->api_url = ($api_url) ? $api_url : 'http://snapsearch.io/api/v1/robot';
+		$this->api = ($api) ? $api : Api::init();
 
 	}
 
-	public function request(){
+	public function request($current_url){
 
+		//the current url must contain the entire url with the _escaped_fragment_ parsed out
+		$this->request_parameters['url'] = $current_url;
+
+		try{
+
+			$response = $this->api
+						->post($this->api_url)
+						->timeout(30)
+						->body($this->request_parameters, 'json')
+						->send()
+						->raw_body;
 		
+		}catch(\Exception $e){
+
+			throw new \Exception('Could not establish a connection to SnapSearch.');
+
+		}
+
+		$response = json_decode($response, true);
+
+		//error checking
+		if($response['code'] == 'success'){
+
+			return $response['content'];
+
+		}elseif($response['code'] == 'validation_error'){
+
+			//means that something was incorrect from the request parameters or the url could not be accessed
+			$this->errors = $response['content'];
+			throw new \Exception('Validation error from SnapSearch. Check your request parameters.');
+
+		}else{
+
+			//system error on SnapSearch, nothing we can do
+			return false;
+
+		}
+		
+	}
+
+	public function get_errors(){
+
+		return $this->errors;
+
 	}
 	
 }
--- src/SnapSearchClientPHP/Interceptor.php
+++ src/SnapSearchClientPHP/Interceptor.php
@@ -4,19 +4,33 @@ namespace SnapSearchClientPHP;
 
 use Detector;
 use Client;
+use Symfony\Component\HttpFoundation\Request;
 
 class Interceptor{
 
-	public function __construct(Detector $detector, Client $client){
+	protected $detector;
+	protected $client;
+	protected $request;
 
+	public function __construct(Client $client, Request $request = null, Detector $detector = null){
+
+		$this->client = $client;
+		$this->request = ($request) : Request::createFromGlobals();
 		$this->detector = ($detector) : new Detector;
-		$this->client = ($client) : new Client;
 
 	}
 
 	public function intercept(){
 
 		if($this->detector->detect()){
+
+			//we need to get the current url
+			//this current url wont' be able to get the hash portion and hence hash bang
+			//however if the meta exists, Google will send the hash bang portion to _escaped_fragment_
+			//we'll need to reconstruct the url if that exists
+			//the value of _escaped_fragment_ will be added to the scheme + host + port + path, then add all subsequent query parameters...
+
+
 			return $this->client->request();
 		}
 
