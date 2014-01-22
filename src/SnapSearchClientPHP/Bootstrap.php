67dd307d8e9f501467d68e3f6e411c3234d6246c
CMCDragonkai <cmcdragonkai@gmail.com>
2014-01-23 00:49:09 +1100

    added Bootstrap for non composer users

---
+++ src/SnapSearchClientPHP/Bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SnapSearchClientPHP;
+
+class Bootstrap{
+
+	public static function register(){
+
+		spl_autoload_register(array('\SnapSearchClientPHP\Bootstrap', 'autoload'));
+
+	}
+
+	public static function autoload($class){
+
+		//path to src folder
+		$src_path = dirname(__DIR__);
+ 
+		$file  = '';
+		$namespace = '';
+ 
+		if($last_namespace_pos = strrpos($class, '\\')){
+ 
+			$namespace = substr($class, 0, $last_namespace_pos);
+			$class = substr($class, $last_namespace_pos + 1);
+			//replace all backslashes with DIRECTORY_SEPARATOR, it adds one more to the end
+			$file = strtr($namespace, '\\', DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+ 
+		}
+		
+		//replace all class names with (_) with DIRECTORY_SEPARATOR
+		$file .= strtr($class, '_', DIRECTORY_SEPARATOR);
+ 
+		if(file_exists($src_path . $file . '.php')){
+		
+			require_once($src_path . $file . '.php');
+			return;
+ 
+		}
+
+	}
+
+}
