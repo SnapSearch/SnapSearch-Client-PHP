834c8ae38a948e907314851f02c55957da823040
Roger Qiu <roger.qiu@polycademy.com>
2014-02-13 21:56:44 +1100

    added before_intercept and after_intercept functions to allow client side caching

--- src/SnapSearchClientPHP/Interceptor.php
+++ src/SnapSearchClientPHP/Interceptor.php
@@ -12,6 +12,8 @@ class Interceptor{
 
 	protected $detector;
 	protected $client;
+	protected $before;
+	protected $after;
 
 	/**
 	 * Constructor
@@ -27,6 +29,40 @@ class Interceptor{
 	}
 
 	/**
+	 * Before intercept callback.
+	 * This is intended for client side caching. It can request a client cached resource.
+	 * However it can also be used for other purposes such as logging.
+	 * If the callable returns an array, the array will be used as the returned response for Interceptor::intercept()
+	 * The "callable" typehint is only available php > 5.4
+	 * 
+	 * @param callable $before Anonymous function to be executed before interception
+	 */
+	public function before_intercept($before){
+
+		if(is_callable($before)){
+			$this->before = $before;
+		}
+
+	}
+
+	/**
+	 * After intercept callback.
+	 * This is intended for client side caching. It can store a SnapSearch response as a client cached resource.
+	 * However it can also be used for other purposes such as logging.
+	 * The callable should accept an array parameter which wil be the SnapSearch response
+	 * The "callable" typehint is only available php > 5.4
+	 * 
+	 * @param callable $after Anonymous function to be executed after interception
+	 */
+	public function after_intercept($after){
+
+		if(is_callable($after)){
+			$this->after = $after;
+		}
+
+	}
+
+	/**
 	 * Intercept begins the detection and returns the snapshot if the request was scraped.
 	 * 
 	 * @return array|boolean
@@ -34,8 +70,28 @@ class Interceptor{
 	public function intercept(){
 
 		if($this->detector->detect()){
+
+			//call the before interceptor and return an array response if it has one
+			$before_intercept = $this->before;
+			if($before_intercept){
+				$result = $before_intercept();
+				if(is_array($result)){
+					return $result;
+				}
+			}
+
 			$raw_current_url = $this->detector->get_encoded_url();
-			return $this->client->request($raw_current_url);
+
+			$response = $this->client->request($raw_current_url);
+
+			//call the after response interceptor, and pass in the $response array (which is always going to be an array)
+			$after_intercept = $this->after;
+			if($after_intercept){
+				$after_intercept($response);
+			}
+
+			return $response;
+
 		}
 
 		return false;
--- tests/unit/SnapSearchClientPHP/InterceptorTest.php
+++ tests/unit/SnapSearchClientPHP/InterceptorTest.php
@@ -8,7 +8,23 @@ class InterceptorTest extends \Codeception\TestCase\Test{
 
 	protected $interceptor;
 
+	protected $response_array = array(
+		'status'		=> 200,
+		'headers'		=> array(
+			array(
+				'name'	=> 'Date',
+				'value'	=> 'Tue, 19 Nov 2013 18:23:41 GMT'
+			)
+		),
+		'html'			=> '<html>Hi!</html>',
+		'screenshot'	=> '',
+		'date'			=> '324836',
+		'cache'			=> false
+	);
+
 	public function _before(){
+
+		$response_array = $this->response_array;
 
 		$detector = Stub::make('SnapSearchClientPHP\Detector', array(
 			'detect'	=> function(){
@@ -20,19 +36,8 @@ class InterceptorTest extends \Codeception\TestCase\Test{
 		));
 
 		$client = Stub::make('SnapSearchClientPHP\Client', array(
-			'request'	=> function($url){
-				return array(
-					'status'		=> 200,
-					'headers'		=> array(
-						array(
-							'name'	=> 'Date',
-							'value'	=> 'Tue, 19 Nov 2013 18:23:41 GMT'
-						)
-					),
-					'html'			=> '<html>Hi!</html>',
-					'screenshot'	=> '',
-					'date'			=> '324836',
-				);
+			'request'	=> function($url) use ($response_array){
+				return $response_array;
 			}
 		));
 
@@ -40,10 +45,85 @@ class InterceptorTest extends \Codeception\TestCase\Test{
 
 	}
 
+	public function testBeforeInterceptCallableThatReturnsAnArrayWillBeTheResponseToInterception(){
+
+		$response_array = array(
+			'test' => 'value'
+		);
+
+		$this->interceptor->before_intercept(function() use ($response_array){
+			return $response_array;
+		});
+
+		$content = $this->interceptor->intercept();
+
+		$this->assertEquals($content, $response_array);
+
+	}
+
+	public function testBeforeInterceptCallableThatDoesNotReturnAnArrayWillNotBeTheResponseToInterception(){
+
+		$response_string = 'i will not be the response!';
+
+		$this->interceptor->before_intercept(function() use ($response_string){
+			return $response_string;
+		});
+
+		$content = $this->interceptor->intercept();
+
+		$this->assertNotEquals($content, $response_string);
+
+	}
+
+	public function testAfterInterceptCallableShouldReceiveResponseArray(){
+
+		$after_intercept_response_array = false;
+
+		//late binding so it's by reference
+		$this->interceptor->after_intercept(function($response_array) use (&$after_intercept_response_array){
+			$after_intercept_response_array = $response_array;
+		});
+
+		$content = $this->interceptor->intercept();
+
+		$this->assertEquals($after_intercept_response_array, $content);
+		$this->assertEquals($after_intercept_response_array, $this->response_array);
+
+	}
+
+	public function testAfterInterceptCallableDoesNotNeedToAcceptParameters(){
+
+		$after_intercept = false;
+
+		//late binding so it's by reference
+		$this->interceptor->after_intercept(function() use (&$after_intercept){
+			$after_intercept = 'whateveriwant';
+		});
+
+		$this->interceptor->intercept();
+
+		$this->assertEquals($after_intercept, 'whateveriwant');
+
+	}
+
+	public function testBeforeAndEmptyInterceptCallablesHaveToBeCallablesToBeCalled(){
+
+		$this->interceptor->before_intercept('not a callable');
+		$this->interceptor->after_intercept('not a callable');
+
+		$content = $this->interceptor->intercept();
+
+		$this->assertInternalType('array', $content);
+		$this->assertEquals($content, $this->response_array);
+
+	}
+
 	public function testInterception(){
 
 		$content = $this->interceptor->intercept();
+
 		$this->assertInternalType('array', $content);
+		$this->assertEquals($content, $this->response_array);
 
 	}
 
