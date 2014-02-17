bb27c84f16b21391fc53f116233a873ae5216045
Roger Qiu <roger.qiu@polycademy.com>
2014-02-18 07:38:10 +1100

    refactoring DetectorTest

--- tests/unit/SnapSearchClientPHP/DetectorTest.php
+++ tests/unit/SnapSearchClientPHP/DetectorTest.php
@@ -6,200 +6,28 @@ use Symfony\Component\HttpFoundation\Request;
  
 class DetectorTest extends \Codeception\TestCase\Test{
 
-	protected $normal_browser = array( 
-		'_GET'		=> array(),
-		'_SERVER'	=> array(
-			'HTTP_HOST' => 'localhost', 
-			'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0', 
-			'SERVER_NAME' => 'localhost', 
-			'SERVER_PORT' => '80', 
-			'REMOTE_ADDR' => '::1', 
-			'DOCUMENT_ROOT' => 'C:/www', 
-			'REQUEST_SCHEME' => 'http', 
-			'GATEWAY_INTERFACE' => 'CGI/1.1', 
-			'SERVER_PROTOCOL' => 'HTTP/1.1', 
-			'REQUEST_METHOD' => 'GET', 
-			'QUERY_STRING' => '', 
-			'REQUEST_URI' => '/snapsearch/', 
-		)
-	);
-
-	protected $search_engine = array( 
-		'_GET'		=> array(),
-		'_SERVER'	=> array(
-			'HTTP_HOST' => 'localhost', 
-			'HTTP_USER_AGENT' => 'AdsBot-Google ( http://www.google.com/adsbot.html)', 
-			'SERVER_NAME' => 'localhost', 
-			'SERVER_PORT' => '80', 
-			'REMOTE_ADDR' => '::1', 
-			'DOCUMENT_ROOT' => 'C:/www', 
-			'REQUEST_SCHEME' => 'http', 
-			'GATEWAY_INTERFACE' => 'CGI/1.1', 
-			'SERVER_PROTOCOL' => 'HTTP/1.1', 
-			'REQUEST_METHOD' => 'GET', 
-			'QUERY_STRING' => '', 
-			'REQUEST_URI' => '/snapsearch/', 
-		)
-	);
-
-	protected $snapsearch_robot = array( 
-		'_GET'	=> array(),
-		'_SERVER'	=> array(
-			'HTTP_HOST' => 'localhost', 
-			'HTTP_USER_AGENT' => 'SnapSearch', 
-			'SERVER_NAME' => 'localhost', 
-			'SERVER_PORT' => '80', 
-			'REMOTE_ADDR' => '::1', 
-			'DOCUMENT_ROOT' => 'C:/www', 
-			'REQUEST_SCHEME' => 'http', 
-			'GATEWAY_INTERFACE' => 'CGI/1.1', 
-			'SERVER_PROTOCOL' => 'HTTP/1.1', 
-			'REQUEST_METHOD' => 'GET', 
-			'QUERY_STRING' => '', 
-			'REQUEST_URI' => '/snapsearch/', 
-		)
-	);
-
-	protected $non_get_route = array(
-		'_GET'		=> array(),
-		'_SERVER'	=> array(
-			'HTTP_HOST' => 'localhost', 
-			'HTTP_USER_AGENT' => 'AdsBot-Google ( http://www.google.com/adsbot.html)', 
-			'SERVER_NAME' => 'localhost', 
-			'SERVER_PORT' => '80', 
-			'REMOTE_ADDR' => '::1', 
-			'DOCUMENT_ROOT' => 'C:/www', 
-			'REQUEST_SCHEME' => 'http', 
-			'GATEWAY_INTERFACE' => 'CGI/1.1', 
-			'SERVER_PROTOCOL' => 'HTTP/1.1', 
-			'REQUEST_METHOD' => 'POST', 
-			'QUERY_STRING' => '', 
-			'REQUEST_URI' => '/snapsearch/', 
-		)
-	);
-
-	protected $ignored_route = array ( 
-		'_GET'		=> array(),
-		'_SERVER'	=> array(
-			'HTTP_HOST' => 'localhost', 
-			'HTTP_USER_AGENT' => 'Googlebot-Video/1.0', 
-			'SERVER_NAME' => 'localhost', 
-			'SERVER_PORT' => '80', 
-			'REMOTE_ADDR' => '::1', 
-			'DOCUMENT_ROOT' => 'C:/www', 
-			'REQUEST_SCHEME' => 'http', 
-			'GATEWAY_INTERFACE' => 'CGI/1.1', 
-			'SERVER_PROTOCOL' => 'HTTP/1.1', 
-			'REQUEST_METHOD' => 'GET', 
-			'QUERY_STRING' => '', 
-			'REQUEST_URI' => '/ignored/', 
-		)
-	);
-
-	protected $matched_route = array( 
-		'_GET'		=> array(),
-		'_SERVER'	=> array(
-			'HTTP_HOST' => 'localhost', 
-			'HTTP_USER_AGENT' => 'msnbot/1.1 ( http://search.msn.com/msnbot.htm)', 
-			'SERVER_NAME' => 'localhost', 
-			'SERVER_PORT' => '80', 
-			'REMOTE_ADDR' => '::1', 
-			'DOCUMENT_ROOT' => 'C:/www', 
-			'REQUEST_SCHEME' => 'http', 
-			'GATEWAY_INTERFACE' => 'CGI/1.1', 
-			'SERVER_PROTOCOL' => 'HTTP/1.1', 
-			'REQUEST_METHOD' => 'GET', 
-			'QUERY_STRING' => '', 
-			'REQUEST_URI' => '/matched/', 
-		)
-	);
-
-	protected $invalid_file_extension = array(
-		'_GET'		=> array(),
-		'_SERVER'	=> array(
-			'HTTP_HOST' => 'localhost', 
-			'HTTP_USER_AGENT' => 'AdsBot-Google ( http://www.google.com/adsbot.html)', 
-			'SERVER_NAME' => 'localhost', 
-			'SERVER_PORT' => '80', 
-			'REMOTE_ADDR' => '::1', 
-			'DOCUMENT_ROOT' => 'C:/www', 
-			'REQUEST_SCHEME' => 'http', 
-			'GATEWAY_INTERFACE' => 'CGI/1.1', 
-			'SERVER_PROTOCOL' => 'HTTP/1.1', 
-			'REQUEST_METHOD' => 'GET', 
-			'QUERY_STRING' => '', 
-			'REQUEST_URI' => '/snapsearch/song.html.mp3?key=value', 
-		)
-	);
-
-	protected $valid_file_extension = array(
-		'_GET'		=> array(),
-		'_SERVER'	=> array(
-			'HTTP_HOST' => 'localhost', 
-			'HTTP_USER_AGENT' => 'AdsBot-Google ( http://www.google.com/adsbot.html)', 
-			'SERVER_NAME' => 'localhost', 
-			'SERVER_PORT' => '80', 
-			'REMOTE_ADDR' => '::1', 
-			'DOCUMENT_ROOT' => 'C:/www', 
-			'REQUEST_SCHEME' => 'http', 
-			'GATEWAY_INTERFACE' => 'CGI/1.1', 
-			'SERVER_PROTOCOL' => 'HTTP/1.1', 
-			'REQUEST_METHOD' => 'GET', 
-			'QUERY_STRING' => '', 
-			'REQUEST_URI' => '/snapsearch/song.html?key=value', 
-		)
-	);
-
-	protected $basic_escaped_fragment_route = array(
-		'_GET'		=> array(
-			'_escaped_fragment_' => ''
-		),
-		'_SERVER'	=> array(
-			'HTTP_HOST' => 'localhost', 
-			'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0', 
-			'SERVER_NAME' => 'localhost', 
-			'SERVER_PORT' => '80', 
-			'REMOTE_ADDR' => '::1', 
-			'DOCUMENT_ROOT' => 'C:/www', 
-			'REQUEST_SCHEME' => 'http', 
-			'GATEWAY_INTERFACE' => 'CGI/1.1', 
-			'SERVER_PROTOCOL' => 'HTTP/1.1', 
-			'REQUEST_METHOD' => 'GET', 
-			'QUERY_STRING' => '_escaped_fragment_',
-			'REQUEST_URI' => '/snapsearch?_escaped_fragment_',
-		),
-	);
-
-	protected $escaped_fragment_route = array(
-		'_GET'		=> array(
-			'key1' => 'value1', 
-			'_escaped_fragment_' => '/path2?key2=value2'
-		),
-		'_SERVER'	=> array(
-			'HTTP_HOST' => 'localhost', 
-			'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0', 
-			'SERVER_NAME' => 'localhost', 
-			'SERVER_PORT' => '80', 
-			'REMOTE_ADDR' => '::1', 
-			'DOCUMENT_ROOT' => 'C:/www', 
-			'REQUEST_SCHEME' => 'http', 
-			'GATEWAY_INTERFACE' => 'CGI/1.1', 
-			'SERVER_PROTOCOL' => 'HTTP/1.1', 
-			'REQUEST_METHOD' => 'GET', 
-			'QUERY_STRING' => 'key1=value1&_escaped_fragment_=%2Fpath2%3Fkey2=value2',
-			'REQUEST_URI' => '/snapsearch/path1?key1=value1&_escaped_fragment_=%2Fpath2%3Fkey2=value2',
-		),
-	);
-
 	public function testNormalBrowserRequestShouldNotBeIntercepted(){
 
 		$request = new Request(
-			$this->normal_browser['_GET'], 
+			$_GET, 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->normal_browser['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => '', 
+				'REQUEST_URI' => '/snapsearch/', 
+			)
 		);
 
 		$detector = new Detector(null, null, false, $request);
@@ -211,12 +39,25 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testSearchEngineRobotShouldBeIntercepted(){
 
 		$request = new Request(
-			$this->search_engine['_GET'], 
+			$_GET, 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->search_engine['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'AdsBot-Google ( http://www.google.com/adsbot.html)', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => '', 
+				'REQUEST_URI' => '/snapsearch/', 
+			)
 		);
 
 		$detector = new Detector(null, null, false, $request);
@@ -228,12 +69,25 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testSnapSearchRobotShouldNotBeIntercepted(){
 
 		$request = new Request(
-			$this->snapsearch_robot['_GET'], 
+			$_GET, 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->snapsearch_robot['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'SnapSearch', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => '', 
+				'REQUEST_URI' => '/snapsearch/', 
+			)
 		);
 
 		$detector = new Detector(null, null, false, $request);
@@ -245,12 +99,25 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testNonGetRequestsShouldNotBeIntercepted(){
 
 		$request = new Request(
-			$this->non_get_route['_GET'], 
+			$_GET, 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->non_get_route['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'AdsBot-Google ( http://www.google.com/adsbot.html)', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'POST', 
+				'QUERY_STRING' => '', 
+				'REQUEST_URI' => '/snapsearch/', 
+			)
 		);
 
 		$detector = new Detector(null, null, false, $request);
@@ -262,12 +129,25 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testIgnoredRoutesShouldNotBeIntercepted(){
 
 		$request = new Request(
-			$this->ignored_route['_GET'], 
+			$_GET, 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->ignored_route['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'Googlebot-Video/1.0', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => '', 
+				'REQUEST_URI' => '/ignored/', 
+			)
 		);
 
 		$detector = new Detector(array('^\/ignored'), null, false, $request);
@@ -279,12 +159,25 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testNonMatchedRoutesShouldNotBeIntercepted(){
 
 		$request = new Request(
-			$this->matched_route['_GET'], 
+			$_GET, 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->matched_route['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'msnbot/1.1 ( http://search.msn.com/msnbot.htm)', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => '', 
+				'REQUEST_URI' => '/matched/', 
+			)
 		);
 
 		$detector = new Detector(null, array('^\/non_matched_route'), false, $request);
@@ -296,12 +189,25 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testMatchedRoutesShouldBeIntercepted(){
 
 		$request = new Request(
-			$this->matched_route['_GET'], 
+			$_GET, 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->matched_route['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'msnbot/1.1 ( http://search.msn.com/msnbot.htm)', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => '', 
+				'REQUEST_URI' => '/matched/', 
+			)
 		);
 
 		$detector = new Detector(null, array('^\/matched'), false, $request);
@@ -313,12 +219,25 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testValidFileExtensionsShouldBeInterceptedIfOtherFactorsAllowIt(){
 
 		$request = new Request(
-			$this->valid_file_extension['_GET'], 
+			$_GET, 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->valid_file_extension['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'AdsBot-Google ( http://www.google.com/adsbot.html)', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => '', 
+				'REQUEST_URI' => '/snapsearch/song.html?key=value', 
+			)
 		);
 
 		$detector = new Detector(null, null, true, $request);
@@ -330,12 +249,25 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testInvalidFileExtensionsShouldNotBeIntercepted(){
 
 		$request = new Request(
-			$this->invalid_file_extension['_GET'], 
+			$_GET, 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->invalid_file_extension['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'AdsBot-Google ( http://www.google.com/adsbot.html)', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => '', 
+				'REQUEST_URI' => '/snapsearch/song.html.mp3?key=value', 
+			)
 		);
 
 		$detector = new Detector(null, null, true, $request);
@@ -347,12 +279,25 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testNonExistentFileExtensionShouldBeInterceptedIfOtherFactorsAllowIt(){
 
 		$request = new Request(
-			$this->search_engine['_GET'], 
+			$_GET, 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->search_engine['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'AdsBot-Google ( http://www.google.com/adsbot.html)', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => '', 
+				'REQUEST_URI' => '/snapsearch/', 
+			)
 		);
 
 		$detector = new Detector(null, null, true, $request);
@@ -364,12 +309,27 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testEscapedFragmentRouteShouldBeIntercepted(){
 
 		$request = new Request(
-			$this->basic_escaped_fragment_route['_GET'], 
+			array(
+				'_escaped_fragment_' => ''
+			), 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->basic_escaped_fragment_route['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => '_escaped_fragment_',
+				'REQUEST_URI' => '/snapsearch?_escaped_fragment_',
+			)
 		);
 
 		$detector = new Detector(null, null, false, $request);
@@ -381,12 +341,28 @@ class DetectorTest extends \Codeception\TestCase\Test{
 	public function testEscapedFragmentRouteShouldBeConvertedBackToHashFragment(){
 
 		$request = new Request(
-			$this->escaped_fragment_route['_GET'], 
+			array(
+				'key1' => 'value1', 
+				'_escaped_fragment_' => '/path2?key2=value2'
+			), 
 			$_POST, 
 			array(), 
 			$_COOKIE, 
 			$_FILES, 
-			$this->escaped_fragment_route['_SERVER']
+			array(
+				'HTTP_HOST' => 'localhost', 
+				'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0', 
+				'SERVER_NAME' => 'localhost', 
+				'SERVER_PORT' => '80', 
+				'REMOTE_ADDR' => '::1', 
+				'DOCUMENT_ROOT' => 'C:/www', 
+				'REQUEST_SCHEME' => 'http', 
+				'GATEWAY_INTERFACE' => 'CGI/1.1', 
+				'SERVER_PROTOCOL' => 'HTTP/1.1', 
+				'REQUEST_METHOD' => 'GET', 
+				'QUERY_STRING' => 'key1=value1&_escaped_fragment_=%2Fpath2%3Fkey2=value2',
+				'REQUEST_URI' => '/snapsearch/path1?key1=value1&_escaped_fragment_=%2Fpath2%3Fkey2=value2',
+			)
 		);
 
 		$detector = new Detector(null, null, false, $request);
